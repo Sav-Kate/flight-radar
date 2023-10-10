7232a94816b029dea2912fae05edf99713728548
Sav-Kate <katsiarynasav@gmail.com>
2023-10-10 21:21:24 +0300

    check if flight is multi-leg

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,8 @@ class Flight < ApplicationRecord
     has_many :flight_legs
     has_many :departure_airports, through: :flight_legs
     has_many :arrival_airports, through: :flight_legs
+
+    def is_flight_multi_leg?
+        return flight_legs.size > 1 ? true : false
+    end
 end
